6ff93bfe45530b271dbed64e753c31e6c1807d3d
Matías Battocchia <matias@riseup.net>
2023-08-08 14:39:02 -0300

    de/serializing based on content type and accept headers

--- src/deserve.py
+++ src/deserve.py
@@ -1,25 +1,24 @@
 """Deserve is a nanoframework for serving ML models"""
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 import json
 import sys
 
-async def desend(send, response, status=200):
-    payload = json.dumps(response).encode()
+async def desend(send, body, content, status=200):
 
     await send({
         'type': 'http.response.start',
         'status': status,
         'headers': [
-            (b'content-type', b'application/json'),
-            (b'content-length', str(len(payload)).encode()),
+            (b'content-type', content),
+            (b'content-length', str(len(body)).encode()),
         ],
     })
 
     await send({
         'type': 'http.response.body',
-        'body': payload,
+        'body': body,
     })
 
 def deserve(app):
@@ -28,24 +27,45 @@ def deserve(app):
         # if scope['type'] != 'http':
         #     return
         #
-        # if (b'content-type', b'application/json') not in scope['headers']:
-        #     return
+
+        headers = dict(scope['headers'])
+        content = headers.get(b'content-type', b'')
+        accept  = headers.get(b'accept', b'')
 
         event = await receive()
 
         # if event['type'] != 'http.request':
         #     return
 
-        try:
-            request = json.loads(event['body']) if event['body'] else None
+        if b'text/' in content:
+            request = event['body'].decode()
+        elif b'image/' in content or b'audio/' in content:
+            request = event['body']
+        else:
+            try:
+                request = json.loads(event['body'])
+            except json.JSONDecodeError as e:
+                if content == b'application/json':
+                    return await desend(
+                        send,
+                        json.dumps({ 'error': f'JSON decode error. {e}' }).encode(),
+                        content,
+                        status=400
+                    )
 
-        except json.JSONDecodeError as e:
-            response = { 'error': f'JSON decode error. {e}' }
-            return await desend(send, response, 400)
+                request = event['body']
 
         response = await app(request)
 
-        await desend(send, response)
+        if b'text/' in accept:
+            await desend(send, response.encode(), content=accept)
+        elif b'image/' in accept or b'audio/' in accept:
+            await desend(send, response, content=accept)
+        else:
+            if accept == b'application/json' or type(response) not in [bytes, bytearray]:
+                await desend(send, json.dumps(response).encode(), content=accept)
+            else:
+                await desend(send, response, content=b'application/octet-stream')
 
     return wrapper
 
